82ba08bc14a6b0329c70b2bef8235f0934446a3f
ejansen927 <154397323+ejansen927@users.noreply.github.com>
2026-02-02 19:00:26 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,47 +1,5 @@
 #!/bin/bash
-#
-"""
-# Run 1: AL uncertainty + random candidates
-python src/train.py \
-    model=gnn \
-    data=al_uncertainty1 \
-    model.num_layers=4 \
-    model.hidden_dim=256 \
-    optimizer=adamw \
-    optimizer.lr=1.5e-3 \
-    optimizer.weight_decay=1e-3 \
-    training.epochs=100 \
-    training.batch_size=256 \
-    project.name=VDAT-ML \
-    project.experiment=gnn_al_uncertainty1
-
-# Run 2: AL uncertainty + clipped candidates
-python src/train.py \
-    model=gnn \
-    data=al_uncertainty2 \
-    model.num_layers=4 \
-    model.hidden_dim=256 \
-    optimizer=adamw \
-    optimizer.lr=1.5e-3 \
-    optimizer.weight_decay=1e-3 \
-    training.epochs=100 \
-    training.batch_size=256 \
-    project.name=VDAT-ML \
-    project.experiment=gnn_al_uncertainty2
-
-python src/train.py \
-    model=gnn \
-    data=hybrid_sweep_100k_graph \
-    model.num_layers=4 \
-    model.hidden_dim=256 \
-    model.dropout=0.1 \
-    optimizer=adamw \
-    optimizer.lr=1.5e-3 \
-    optimizer.weight_decay=1e-3 \
-    training.epochs=100 \
-    training.batch_size=256 \
-    project.name=VDAT-ML \
-    project.experiment=gnn_hybrid_sweep"""
+# my sample tests
 
 python src/train.py -m \
     model=gnn \
